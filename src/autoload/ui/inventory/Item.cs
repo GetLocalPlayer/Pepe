082d12daf479025f81f7f7594402521d09a2fc73
GetLocalPlayer <GetLocalPlayer@gmail.com>
2024-01-31 00:17:17 +0400

    Implemented unique ViewportTexture and World3D for corresponding nodes of Item class

--- src/autoload/ui/inventory/Item.cs
+++ src/autoload/ui/inventory/Item.cs
@@ -8,6 +8,21 @@ public partial class Item : MarginContainer
 {
     [Export] public string Description = null;
 
+    uint _amount = 0;
+    [Export] public uint Amount
+    {
+        get => _amount;
+        set {
+            _amount = value;
+            if (IsNodeReady())
+            {
+                 GetNode<Label>("Texture/Amount").Text = $"x{value}";
+                if (!Engine.IsEditorHint())
+                    Visible = value > 0;
+            }
+        }
+    }
+
     public Dictionary<string, Action<Item>> Actions = new(){};
 
     PackedScene _model;
@@ -17,14 +32,11 @@ public partial class Item : MarginContainer
         set {
             if (IsNodeReady())
             {
-                if (_model != value)
-                {   
-                    var modelOwner = GetNode<Node3D>("SubViewport/ModelOwner");
-                    foreach (var child in modelOwner.GetChildren())
-                        child.QueueFree();
-                    if (value != null)
-                        modelOwner.AddChild(value.Instantiate());
-                }
+                var modelOwner = GetNode<Node3D>("SubViewport/ModelOwner");
+                foreach (var child in modelOwner.GetChildren())
+                    child.QueueFree();
+                if (value != null)
+                    modelOwner.AddChild(value.Instantiate());
             }
             _model = value;
         }
@@ -61,6 +73,17 @@ public partial class Item : MarginContainer
 
     public override void _Ready()
     {
+        var vp = GetNode<SubViewport>("SubViewport");
+        var texRect = GetNode<TextureRect>("Texture");
+        texRect.Texture = vp.GetTexture();
+
+        if (!Engine.IsEditorHint())
+            Visible = Amount > 0;
+
+        var animPlayer = GetNode<AnimationPlayer>("SubViewport/AnimationPlayer");
+        animPlayer.Play(animPlayer.Autoplay);
+
+
         var modelOwner = GetNode<Node3D>("SubViewport/ModelOwner");
         modelOwner.Scale = new Vector3(ModelScale, ModelScale, ModelScale);
         modelOwner.Position = ModelOffset;
@@ -68,7 +91,5 @@ public partial class Item : MarginContainer
         {
             modelOwner.AddChild(Model.Instantiate());
         }
-        var animPlayer = GetNode<AnimationPlayer>("SubViewport/AnimationPlayer");
-        animPlayer.Play(animPlayer.Autoplay);
     }
 }
