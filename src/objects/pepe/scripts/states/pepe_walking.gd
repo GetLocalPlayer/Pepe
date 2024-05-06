713ac812af210814982252aea6dda9d256da8b03
GetLocalPlayer <GetLocalPlayer@gmail.com>
2024-05-06 14:22:21 +0400

    Unused code due inheritence

--- src/objects/pepe/scripts/states/pepe_walking.gd
+++ src/objects/pepe/scripts/states/pepe_walking.gd
@@ -10,13 +10,3 @@ func _enter(context: Node):
 	get_playback(context).travel(animation.states.walking)
 	_handle_input(context, null)
 
-
-func _update(context: Node, delta: float):
-	var body = context as CharacterBody3D;
-	var anim_tree = get_animation_tree(context)
-	var rm_rotation = anim_tree.get_root_motion_rotation()
-	var rm_position = anim_tree.get_root_motion_position()
-	body.quaternion *= rm_rotation
-	body.velocity = (anim_tree.get_root_motion_rotation_accumulator().inverse() * body.quaternion) * rm_position / delta
-	body.move_and_slide()
-
