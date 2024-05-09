330cac36bf46ebbf15ad625fd0820ffcd3c82d4a
GetLocalPlayer <GetLocalPlayer@gmail.com>
2024-05-09 14:17:14 +0400

    Added priority for interactables

--- src/objects/interactable/interactable.gd
+++ src/objects/interactable/interactable.gd
@@ -5,6 +5,7 @@ class_name Interactable
 @export var _lines: Array[String]
 # To avoid conflicts with @tool script in child classes
 @onready var _interaction_ui = null if Engine.is_editor_hint() else get_node("/root/Interaction")
+@export var interaction_priority: int
 
 
 
