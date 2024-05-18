9cf2b1c72537fd46c77bc5c5a23a8ef3d3d20245
GetLocalPlayer <GetLocalPlayer@gmail.com>
2024-05-18 23:22:12 +0400

    Support options for interactables

--- src/objects/interactable/interactable.gd
+++ src/objects/interactable/interactable.gd
@@ -2,7 +2,8 @@ extends Area3D
 class_name Interactable
 
 
-@export var _lines: Array[String]
+@export_multiline var _lines: Array[String]
+@export var _options: Array[String]
 # To avoid conflicts with @tool script in child classes
 @onready var _interaction_ui = null if Engine.is_editor_hint() else get_node("/root/Interaction")
 @export var interaction_priority: int
@@ -10,5 +11,8 @@ class_name Interactable
 
 
 func interact():
-	_interaction_ui.run(_lines)
-	await _interaction_ui.finished
+	if _options.is_empty():
+		_interaction_ui.run(_lines)
+	else:
+		_interaction_ui.run_options(_lines, _options)
+	return await _interaction_ui.finished
