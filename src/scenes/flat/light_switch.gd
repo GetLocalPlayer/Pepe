1984ddf2504e04cbd720bab8ae65273487cf1ee4
GetLocalPlayer <GetLocalPlayer@gmail.com>
2024-05-18 23:22:51 +0400

    Added lightswitch for Kitchen in Flat

---
+++ src/scenes/flat/light_switch.gd
@@ -0,0 +1,12 @@
+extends Interactable
+
+
+@onready var _text = _lines[0]
+@onready var _lamp: OmniLight3D = $Lamp
+
+
+func interact():
+	_lines[0] = _text % ("off" if _lamp.visible else "on")
+	print(_lines[0])
+	if await super.interact() == 0:
+		_lamp.visible = not _lamp.visible
