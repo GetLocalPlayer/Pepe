6c8f46d143ecba0007f098593ebcdb03f522abcf
GetLocalPlayer <GetLocalPlayer@gmail.com>
2024-05-18 23:21:46 +0400

    Implemented buttons with options for nteraction

--- src/autoload/ui/Interaction/interaction.gd
+++ src/autoload/ui/Interaction/interaction.gd
@@ -1,34 +1,58 @@
 extends Control
 
 
-signal finished
+signal finished(int)
 
-@onready var _label = $Text
-var _text_lines: Array
+@onready var _label: RichTextLabel = %Text
+@onready var _text_button: Button = %ButtonNext
+@onready var _buttons: Array[Button] = [%Button1, %Button2, %Button3, %Button4]
 
 
 func _on_visibility_changed():
-    get_tree().paused = visible
+	get_tree().paused = visible
 
 
 func _ready():
-    hide()
-    visibility_changed.connect(_on_visibility_changed)
+	hide()
+	visibility_changed.connect(_on_visibility_changed)
+	for btn in _buttons:
+		btn.pressed.connect(_on_button_pressed.bind(btn))
 
 
-func _input(_event):
-    if visible and Input.is_action_just_pressed("Action"):
-        if _text_lines.is_empty():
-            _label.text = ""
-            hide()
-        else:
-            _label.text = _text_lines.pop_back()
+func _on_button_pressed(btn: Button):
+	finished.emit(_buttons.find(btn))
+	hide()
 
 
 func run(lines: Array):
-    if lines.is_empty():
-        push_error("`lines` cannot be empty!")
-    _text_lines = lines.duplicate()
-    _text_lines.reverse()
-    _label.text = _text_lines.pop_back()
-    show()
+	if lines.is_empty():
+		push_error("`lines` cannot be empty!")
+	run_options(lines, [])
+
+
+
+"""
+	Accepts lines and names for buttons.
+"""
+func run_options(lines: Array[String], button_names: Array[String]):
+	for btn in _buttons:
+		btn.hide()
+	show()
+	_text_button.grab_focus()
+	for s in lines:
+		_label.text = "[center]%s" % s
+		await _text_button.pressed
+
+	if button_names.is_empty():
+		finished.emit(-1)
+		hide()
+		return
+	
+	for i in range(button_names.size() if button_names.size() <= _buttons.size() else _buttons.size()):
+		var btn = _buttons[i]
+		btn.show()
+		btn.text = button_names[i]
+
+	_buttons[0].grab_focus()
+	
+	
