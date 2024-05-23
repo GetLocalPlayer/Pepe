74fb30f304897eecf9d393a3d10dc233b4733f2b
GetLocalPlayer <GetLocalPlayer@gmail.com>
2024-05-23 20:18:06 +0400

    Characters appearing over time in description

--- src/autoload/ui/pickup/pickup.gd
+++ src/autoload/ui/pickup/pickup.gd
@@ -5,6 +5,7 @@ signal finished(taken: bool)
 
 
 @export var _item_scale_tween_time = 0.75
+@export var _description_charachters_appearing_rate = 50
 
 @onready var _buttons = {
 	yes = %YesButton,
@@ -61,24 +62,28 @@ func run(item_name: String, amount: int, lines: Array[String]) -> void:
 		return
 		
 	show()
+	_buttons.yes.hide()
+	_buttons.no.hide()
+	_description.hide()
 	
 	for i in _item_list.get_children():
 		i.amount = amount if i.name == item_name else 0
 
-	var tween = create_tween()
+	var tween: Tween = create_tween()
 	tween.tween_property(_item_scroll, "scale", _item_scroll.scale, _item_scale_tween_time)
-	tween.parallel().tween_callback(_buttons.yes.show).set_delay(_item_scale_tween_time)
 	_item_scroll.scale = Vector2(0, 0)
-	_buttons.yes.hide()
-	_buttons.no.hide()
-	_description.hide()
 	await tween.parallel().tween_interval(_item_scale_tween_time).finished
 
 	_description.show()
 	_description_button.grab_focus()
-	for i in range(lines.size() - 1):
-		_description.text = lines[i]
+	for s in lines:
+		_description.text = s
+		_description.visible_characters = 0
+		var t = _description.create_tween()
+		t.tween_property(_description, "visible_characters", s.length(), float(s.length()) / float(_description_charachters_appearing_rate))
 		await _description_button.pressed
+		_description.visible_characters = -1
+		t.stop()
 	_description.text = lines.back()
 	_buttons.yes.show()
 	_buttons.yes.grab_focus()
